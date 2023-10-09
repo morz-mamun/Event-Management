95be17343d92791e6f79b064621f0344e1980d64
morz-mamun <morzmamun@gmail.com>
2023-10-09 20:27:26 +0600

    private route venues data display done

---
+++ src/PrivateRoute/Venues/SingleVenue.jsx
@@ -0,0 +1,33 @@
+
+import PropTypes from 'prop-types';
+import { FaLongArrowAltRight } from 'react-icons/fa';
+
+const SingleVenue = ({venue}) => {
+
+    const {venue_name, img, location, description} = venue
+    return (
+        <div className='md:my-10'>
+            <div className="card card-compact bg-base-100 shadow-xl">
+                <figure><img src={img} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                     <span className='text-red-600'> Venue Name: </span> {venue_name}</h2>
+                    
+                    <p> <span className='text-red-600 font-bold'> Location: </span> {location}</p>
+                    <h2><span className='text-red-600 font-bold'>Description: </span>{description}</h2>
+                    <div className="card-actions">
+                        <button className="btn bg-gradient-to-r from-red-600 to-slate-900  font-bold text-white">Choose Your
+                        <FaLongArrowAltRight></FaLongArrowAltRight>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+SingleVenue.propTypes = {
+
+};
+
+export default SingleVenue;
--- src/PrivateRoute/Venues/Venues.jsx
+++ src/PrivateRoute/Venues/Venues.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import SingleVenue from "./SingleVenue";
 
 
 const Venues = () => {
+    const [allVenue, setAllVenue] = useState([])
+
+    useEffect(() => {
+        fetch('/venue.json')
+        .then(res => res.json())
+        .then(data => setAllVenue(data))
+    },[])
     return (
-        <div>
-            <h1>this is venues page</h1>
+        <div className="px-2 space-y-5 max-w-7xl mx-auto">
+            <h1 className='text-5xl font-bold text-center my-10' data-aos="fade-down">Our <span className='text-red-600'>Venues</span> </h1>
+            <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+            {
+                allVenue.map(venue => <SingleVenue key={venue.id} venue={venue}></SingleVenue>)
+            }
+            </div>
         </div>
     );
 };
